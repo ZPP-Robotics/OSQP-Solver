c766061bdcc704f2895e50bb005d032c3774e70f
barteksad <barteksadlej@gmail.com>
2023-01-18 10:35:05 +0100

    first version with jacobian constraint

--- src/gomp-solver.h
+++ src/gomp-solver.h
@@ -7,7 +7,7 @@
 template<size_t N_DIM>
 class GOMPSolver {
 
-    using Position = std::array<double, N_DIM>;
+    // using Position = std::array<double, N_DIM>;
 
 public:
 
@@ -15,21 +15,23 @@ public:
                Constraint<N_DIM> pos_con,
                Constraint<N_DIM> vel_con,
                Constraint<N_DIM> acc_con,
-               QPMatrix &&P)
+               QPMatrix &&P,
+               std::vector<std::pair<size_t, Constraint<N_DIM>>> z_obstacles_geq)
             : max_waypoints(waypoints),
               time_step(time_step),
               pos_con(pos_con),
               vel_con(vel_con),
               acc_con(acc_con),
-              problem_matrix(P) {
+              problem_matrix(P),
+              z_obstacles_geq(z_obstacles_geq) {
         assert(max_waypoints >= 2);
     }
 
-    std::pair<ExitCode, QPVector> run(Position start_pos, Position end_pos) {
-        auto constraints = initConstraints(start_pos, end_pos);
+    std::pair<ExitCode, QPVector> run(Ctrl start_pos, Ctrl end_pos) {
+        auto [constraints, warm_start] = initConstraintsAndWarmStart(start_pos, end_pos);
 
         auto qp_solver = QPSolver{constraints.build(), problem_matrix};
-
+        qp_solver.setWarmStart(warm_start);
         auto last_code = ExitCode::kUnknown;
         QPVector last_solution{};
 
@@ -48,9 +50,10 @@ public:
             last_solution = solution;
             qp_solver.update(
                     constraints
-                            .position(t - 2, equal<N_DIM>(end_pos))
+                            .position(t - 2, end_pos.toConstraintEq())
                             .velocity(t - 2, EQ_ZERO<N_DIM>)
                             .acceleration(t - 2, EQ_ZERO<N_DIM>)
+                            .zObstacles(z_obstacles_geq, solution)
                             .build()
             );
         }
@@ -58,37 +61,37 @@ public:
         return {last_code, last_solution};
     }
 
-    std::pair<ExitCode, QPVector> run_binsearch(Position start_pos, Position end_pos) {
-        auto qp_solver = QPSolver{initConstraints(start_pos, end_pos).build(), problem_matrix};
-        auto [code, sol] = qp_solver.solve();
-        if (code != ExitCode::kOptimal) {
-            return {code, sol};
-        }
+    // std::pair<ExitCode, QPVector> run_binsearch(Point start_pos, Point end_pos) {
+    //     auto qp_solver = QPSolver{initConstraints(start_pos, end_pos).build(), problem_matrix};
+    //     auto [code, sol] = qp_solver.solve();
+    //     if (code != ExitCode::kOptimal) {
+    //         return {code, sol};
+    //     }
 
-        size_t l = 2;
-        size_t p = max_waypoints - 1;
-        while (l < p) {
-            size_t m = (l + p) / 2;
-            auto constraint_matrix = initConstraints(start_pos, end_pos)
-                    .positions(m - 1, max_waypoints - 1, equal<N_DIM>(end_pos))
-                    .velocities(m - 1, max_waypoints - 1, EQ_ZERO<N_DIM>)
-                    .accelerations(m - 1, max_waypoints - 2, EQ_ZERO<N_DIM>)
-                    .build();
+    //     size_t l = 2;
+    //     size_t p = max_waypoints - 1;
+    //     while (l < p) {
+    //         size_t m = (l + p) / 2;
+    //         auto constraint_matrix = initConstraints(start_pos, end_pos)
+    //                 .positions(m - 1, max_waypoints - 1, end_pos.toConstraintEq())
+    //                 .velocities(m - 1, max_waypoints - 1, EQ_ZERO<N_DIM>)
+    //                 .accelerations(m - 1, max_waypoints - 2, EQ_ZERO<N_DIM>)
+    //                 .build();
 
-            qp_solver.update(constraint_matrix);
-            auto [exit_code, solution] = qp_solver.solve();
+    //         qp_solver.update(constraint_matrix);
+    //         auto [exit_code, solution] = qp_solver.solve();
 
-            if (exit_code == ExitCode::kOptimal) {
-                p = m;
-                sol = solution;
-                code = ExitCode::kOptimal;
-            } else {
-                l = m + 1;
-            }
-        };
-        return {code, sol};
+    //         if (exit_code == ExitCode::kOptimal) {
+    //             p = m;
+    //             sol = solution;
+    //             code = ExitCode::kOptimal;
+    //         } else {
+    //             l = m + 1;
+    //         }
+    //     };
+    //     return {code, sol};
 
-    }
+    // }
 
 private:
 
@@ -98,22 +101,29 @@ private:
     const Constraint<N_DIM> vel_con;
     const Constraint<N_DIM> acc_con;
     const QPMatrix problem_matrix;
+    const std::vector<std::pair<size_t, Constraint<N_DIM>>> z_obstacles_geq;
 
-    ConstraintBuilder<N_DIM> initConstraints(Position start_pos, Position end_pos) {
+    std::pair<ConstraintBuilder<N_DIM>, QPVector> initConstraintsAndWarmStart(const Ctrl& start_pos, const Ctrl& end_pos) {
+        // Warm start as described in paper
+        // Propertes = 2
+        QPVector warm_start = linspace<N_DIM, 2>(start_pos, end_pos, max_waypoints);
+
         // Constrain all positions, velocities, accelerations with
         // pos_con, vel_con, acc_con respectively.
         // Set start and end positions.
         // Set start and end velocities/accelerations to zero.
-        return ConstraintBuilder<N_DIM>{max_waypoints, time_step}
+        return {ConstraintBuilder<N_DIM>{max_waypoints, time_step}
                 .positions(0, max_waypoints - 1, pos_con)
                 .velocities(0, max_waypoints - 1, vel_con)
                 .accelerations(0, max_waypoints - 2, acc_con)
-                .position(0, equal<N_DIM>(start_pos))
+                .position(0, start_pos.toConstraintEq())
                 .velocity(0, EQ_ZERO<N_DIM>)
                 .acceleration(0, EQ_ZERO<N_DIM>)
-                .position(max_waypoints - 1, equal<N_DIM>(end_pos))
+                .position(max_waypoints - 1, end_pos.toConstraintEq())
                 .velocity(max_waypoints - 1, EQ_ZERO<N_DIM>)
-                .acceleration(max_waypoints - 2, EQ_ZERO<N_DIM>);
+                .acceleration(max_waypoints - 2, EQ_ZERO<N_DIM>)
+                .zObstacles(z_obstacles_geq, warm_start),
+                warm_start};
     }
 
 };
--- src/utils.h
+++ src/utils.h
@@ -3,7 +3,13 @@
 
 #include <limits>
 #include <array>
+#include <iostream>
 #include <osqp++.h>
+
+#include "constraints/constraints.h"
+#include "../../Kinematics-UR5e-arm/src/analytical_ik.h"
+
+#include <armadillo>
 
 using ExitCode = osqp::OsqpExitCode;
 using QPMatrix = Eigen::SparseMatrix<double, Eigen::ColMajor, long long>;
@@ -33,6 +39,103 @@ QPMatrix triDiagonalMatrix(double a, double b, int n, int offset = 0, int diagon
     return m;
 }
 
+struct Point;
+struct Ctrl;
 
+struct Point {
+    double x, y, z;
+    Point(double x, double y, double z) : x(x), y(y), z(z) {}
+    Ctrl toCtrl() const;
+};
+
+struct Ctrl {
+    double q1, q2, q3, q4, q5, q6;
+    Ctrl(double q1, double q2, double q3, double q4, double q5, double q6) 
+    : q1(q1), q2(q2), q3(q3), q4(q4), q5(q5), q6(q6) {}
+    Point toPoint() const;
+
+    constraints::Constraint<6> toConstraintEq() const {
+        return constraints::equal(std::array<double, 6>{q1, q2, q3, q4, q5, q6});
+    }
+
+    QPVector to_vec() const {
+        return QPVector{{q1, q2, q3, q4, q5, q6}};
+    }
+};
+
+// not important stuff, printing etc.
+std::ostream& operator<<(std::ostream& os, const Point& p) {
+    os << "(" << p.x << ", " << p.y << ", " << p.z << ")";
+    return os;
+}
+
+std::string to_string(Point const& arg)
+{
+    std::ostringstream ss;
+    ss << arg;
+    return std::move(ss).str();  // enable efficiencies in c++17
+}
+
+class NoInverseKinematicSolution: public std::runtime_error {
+    public:
+        NoInverseKinematicSolution(Point p) 
+            : std::runtime_error("No inverse kinematic solution exists for point " + to_string(p)) {}
+};
+
+// Converts site_xpos to joint angles
+Ctrl Point::toCtrl() const {
+    double q_sols[8 * 6];
+    int num_sols = inverse_kinematics(q_sols, x, y, z);
+
+    if (num_sols == 0) {
+        throw NoInverseKinematicSolution(*this);
+    }
+
+    // for(auto i = 0; i < num_sols; i++) {
+    //     std::cout << "\nSolution nr: " << i << "\n";
+    //     std::cout << q_sols[i*6 + 0] << ", " << q_sols[i*6 + 1] << ", " << q_sols[i*6 + 2] << ", " << q_sols[i*6 + 3] << ", " << q_sols[i*6 + 4] << ", " << q_sols[i*6 + 5] << "\n";
+    //     std::cout << "As point: " << Ctrl(q_sols[i*6 + 0], q_sols[i*6 + 1], q_sols[i*6 + 2], q_sols[i*6 + 3], q_sols[i*6 + 4], q_sols[i*6 + 5]).toPoint() << "\n";
+    //     double jacobian[18];
+    //     double q[6]{q_sols[i*6 + 0], q_sols[i*6 + 1], q_sols[i*6 + 2], q_sols[i*6 + 3], q_sols[i*6 + 4], q_sols[i*6 + 5]};
+    //     joint_jacobian(jacobian, q);
+    //     arma::mat jacobian_arma = arma::mat{jacobian, 3, 6};
+    //     arma::vec xyz = jacobian_arma * arma::vec{q_sols[i*6 + 0], q_sols[i*6 + 1], q_sols[i*6 + 2], q_sols[i*6 + 3], q_sols[i*6 + 4], q_sols[i*6 + 5]};
+    //     std::cout << "Jacobian: " << Point(xyz[0], xyz[1], xyz[2]) << "\n";
+    // }
+
+    return Ctrl(q_sols[0], q_sols[1], q_sols[2], q_sols[3], q_sols[4], q_sols[5]);
+}
+
+// Converts joint angles to site_xpos (x, y, z)
+Point Ctrl::toPoint() const {
+    double q[6]{q1, q2, q3, q4, q5, q6};
+    std::tuple<double, double, double> fc = forward_kinematics(q);
+    return Point(std::get<0>(fc), std::get<1>(fc), std::get<2>(fc));
+}
+
+template<size_t N_DIM, size_t PROPERTIES>
+QPVector linspace(const Ctrl& a, const Ctrl& b, const size_t n_steps) {
+    QPVector acc;
+    acc.resize(N_DIM * n_steps * PROPERTIES);
+    QPVector step_size = (b.to_vec() - a.to_vec()) / (n_steps - 1);
+    QPVector a_vec = a.to_vec();
+
+    // std::cout << a.to_vec()[0] << ", " << a.to_vec()[1] << ", " << a.to_vec()[2] << ", " << a.to_vec()[3] << ", " << a.to_vec()[4] << ", " << a.to_vec()[5] << "\n";
+    for(auto step = 0; step < n_steps; step++) {
+        QPVector current = step_size * step + a_vec;
+        for(auto i = 0; i < N_DIM; i++) {
+            acc[step * N_DIM + i] = current[i];
+            // std::cout << current[i] << ", ";
+        }
+        // std::cout << "\n";
+    }
+    
+    // std::cout << b.to_vec()[0] << ", " << b.to_vec()[1] << ", " << b.to_vec()[2] << ", " << b.to_vec()[3] << ", " << b.to_vec()[4] << ", " << b.to_vec()[5] << "\n";
+    // exit(1);
+
+    
+    // acc[N_DIM * n_steps: ] = 0, this is warm start for velocity because we do not care
+    return acc;
+}
 
 #endif //UTILS_H
