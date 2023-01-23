b8e6e683f032e88414aafad19923bf49498e44fd
Mateusz Sulimowicz <mateusz.sulimowicz01@gmail.com>
2023-01-23 22:27:00 +0100

    Implement Line class to simulate obstacle's edge

--- src/utils.h
+++ src/utils.h
@@ -41,14 +41,18 @@ struct Ctrl;
 
 struct Point {
     double x, y, z;
+
     Point(double x, double y, double z) : x(x), y(y), z(z) {}
+
     Ctrl toCtrl() const;
 };
 
 struct Ctrl {
     double q1, q2, q3, q4, q5, q6;
-    Ctrl(double q1, double q2, double q3, double q4, double q5, double q6) 
-    : q1(q1), q2(q2), q3(q3), q4(q4), q5(q5), q6(q6) {}
+
+    Ctrl(double q1, double q2, double q3, double q4, double q5, double q6)
+            : q1(q1), q2(q2), q3(q3), q4(q4), q5(q5), q6(q6) {}
+
     Point toPoint() const;
 
     constraints::Constraint<6> toConstraintEq() const {
@@ -60,26 +64,25 @@ struct Ctrl {
     }
 };
 
-std::ostream& operator<<(std::ostream& os, const Point& p) {
+std::ostream &operator<<(std::ostream &os, const Point &p) {
     os << "Point: (" << p.x << ", " << p.y << ", " << p.z << ")";
     return os;
 }
 
-std::ostream& operator<<(std::ostream& os, const Ctrl& p) {
+std::ostream &operator<<(std::ostream &os, const Ctrl &p) {
     os << "Ctrl: (" << p.q1 << ", " << p.q2 << ", " << p.q3 << ", " << p.q4 << ", " << p.q5 << ", " << p.q6 << ")";
     return os;
 }
 
-std::string to_string(Point const& arg)
-{
+std::string to_string(Point const &arg) {
     std::ostringstream ss;
     ss << arg;
     return std::move(ss).str();  // enable efficiencies in c++17
 }
 
-class NoInverseKinematicSolution: public std::runtime_error {
-    public:
-        NoInverseKinematicSolution(Point p) 
+class NoInverseKinematicSolution : public std::runtime_error {
+public:
+    NoInverseKinematicSolution(Point p)
             : std::runtime_error("No inverse kinematic solution exists for point " + to_string(p)) {}
 };
 
@@ -109,28 +112,81 @@ Point Ctrl::toPoint() const {
 }
 
 template<size_t N_DIM, size_t PROPERTIES>
-QPVector linspace(const Ctrl& a, const Ctrl& b, const size_t n_steps) {
+QPVector linspace(const Ctrl &a, const Ctrl &b, const size_t n_steps) {
     QPVector acc;
     acc.resize(N_DIM * n_steps * PROPERTIES);
     QPVector step_size = (b.to_vec() - a.to_vec()) / (n_steps - 1);
     QPVector a_vec = a.to_vec();
 
     // first half is for position, initialize it to linear interpolation from a to b
-    for(auto step = 0; step < n_steps; step++) {
+    for (auto step = 0; step < n_steps; step++) {
         QPVector current = step_size * step + a_vec;
-        for(auto i = 0; i < N_DIM; i++) {
+        for (auto i = 0; i < N_DIM; i++) {
             acc[step * N_DIM + i] = current[i];
         }
     }
 
     // second part if for velocity, initialize it to zero
-    for(auto step = n_steps; step < 2 * n_steps; step++) {
-        for(auto i = 0; i < N_DIM; i++) {
+    for (auto step = n_steps; step < 2 * n_steps; step++) {
+        for (auto i = 0; i < N_DIM; i++) {
             acc[step * N_DIM + i] = 0.0;
         }
     }
-    
+
     return acc;
 }
+
+class Line {
+
+    /**
+     * A - point on the line
+     * D - direction of line (unit length)
+     */
+    const arma::vec3 D;
+    const arma::vec3 A;
+
+public:
+
+    Line(const arma::vec3 &direction, const arma::vec3 &point)
+            : D(direction / arma::norm(direction)), A(point) {}
+
+    /**
+     * P - some point
+     *
+     *     P
+     *    /|
+     *   / |
+     *  /  v
+     * A---X----->D
+     *
+     * X - base of the perpendicular line
+     *
+     * (P-A) dot D == |X-A|
+     *
+     * X == A + ((P-A) dot D)D
+     * Returns perpendicular: X-P
+     */
+    [[nodiscard]] arma::vec3 distanceVec(const arma::vec3 &P) const {
+        arma::vec3 X = A + arma::dot(P - A, D) * D;
+        return X - P;
+    }
+
+    /**
+     * Return "horizontal" distance from P to line.
+     */
+    [[nodiscard]] double distanceXY(const arma::vec3 &P) const {
+        arma::vec3 dist = distanceVec(P);
+        return arma::norm(arma::vec2{dist[0], dist[1]});
+    }
+
+    /**
+     * Return point on the line closest to P.
+     */
+    arma::vec3 operator[](const arma::vec3 &P) const {
+        return P + distanceVec(P);
+    };
+
+};
+
 
 #endif //UTILS_H
