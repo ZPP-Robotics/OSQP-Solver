7b360d9deb9271fcf3527436bf60796b7543907d
Olaf <olafplacha@gmail.com>
2023-03-09 09:28:01 +0100

    specified wrapper return type

---
+++ src/gomp-solver-wrapper.h
@@ -0,0 +1,11 @@
+#ifndef GOMP_SOLVER_WRAPPER_H
+#define GOMP_SOLVER_WRAPPER_H
+
+#include <tuple>
+#include "gomp-solver.h"
+
+using FloatVec = std::array<float>;
+
+std::tuple<FloatVec, FloatVec, FloatVec> run_solver()
+
+#endif // GOMP_SOLVER_WRAPPER_H
