3eb0756f87ae3621a9e9174e090f61e23b9c1280
Olaf <olafplacha@gmail.com>
2023-03-14 20:57:01 +0100

    added position contraints and obstacles to the function constraints

--- src/gomp-solver-wrapper.h
+++ src/gomp-solver-wrapper.h
@@ -4,7 +4,7 @@
 #include <tuple>
 #include "gomp-solver.h"
 
-// def solve(start_pos_joints: Tuple[float x 6], end_pos_tcp: Tuple[float x 3], time_step: float, waypoints_count: int, velocity_constraints: Tuple[List[float x 6], List[float x 6]], acceleration_constraints: Tuple[List[float x 6], List[float x 6]]) -> Tuple[List[float x 6], List[float x 6], List[float x 6]]
+// def solve(start_pos_joints: Tuple[float x 6], end_pos_tcp: Tuple[float x 3], time_step: float, waypoints_count: int, velocity_constraints: Tuple[List[float x 6], List[float x 6]], acceleration_constraints: Tuple[List[float x 6], List[float x 6]], position_constraints: Tuple[List[float x 6], List[float x 6]], obstacles: List[Obstacle]) -> Tuple[List[float x 6], List[float x 6], List[float x 6]]
 
 std::tuple<std::vector<std::array<float, 6>>, std::vector<std::array<float, 6>>, std::vector<std::array<float, 6>>> solve(
     std::array<float, 6> start_pos_joints, 
@@ -12,6 +12,8 @@ std::tuple<std::vector<std::array<float, 6>>, std::vector<std::array<float, 6>>,
     float time_step, 
     int waypoints_count, 
     std::tuple<std::vector<std::array<float, 6>>, std::vector<std::array<float, 6>>> velocity_constraints, 
-    std::tuple<std::vector<std::array<float, 6>>, std::vector<std::array<float, 6>>> acceleration_constraints);
+    std::tuple<std::vector<std::array<float, 6>>, std::vector<std::array<float, 6>>> acceleration_constraints,
+    std::tuple<std::vector<std::array<float, 6>>, std::vector<std::array<float, 6>>> position_constraints,
+    std::vector<Obstacle> obstacles);
 
 #endif // GOMP_SOLVER_WRAPPER_H
