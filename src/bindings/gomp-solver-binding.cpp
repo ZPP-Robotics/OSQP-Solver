da20a83d6a0a19d84fe8ac275c238db07ff9149b
Olaf <olafplacha@gmail.com>
2023-03-22 19:44:01 +0100

    added horizontal line creation

--- src/bindings/gomp-solver-binding.cpp
+++ src/bindings/gomp-solver-binding.cpp
@@ -3,6 +3,7 @@
 #include <array>
 
 #include "../constraints/constraints.h"
+#include "../horizontal-line.h"
 
 #include <pybind11/pybind11.h>
 #include <pybind11/stl.h>
@@ -34,6 +35,32 @@ using point_t = std::tuple<float, float, float>;
 //         return {{{start_pos_joints.at(0),2,3,4,5,6}}, {{1,2,3,4,5,6}}, {{1,2,3,4,5,6}}};
 //     }
 
+constraints::Bound<N_DIM> createBound(const std::array<float, 6>& arr) {
+    const Eigen::Map<const Eigen::Array<float, 6, 1>> arr_map(arr.data());
+
+    const auto double_arr = arr_map.cast<double>();
+    
+    return Eigen::Vector<double, 6>(double_arr);
+}
+
+constraints::Constraint<N_DIM> createConstraint(constraint_t& constraint) {
+    return {createBound(std::get<0>(constraint)), createBound(std::get<1>(constraint))};
+}
+
+std::vector<HorizontalLine> createHorizontalLines(std::vector<std::tuple<point_t, point_t>>& obstacles) {
+    std::vector<HorizontalLine> horizontal_lines;
+    for (auto& obstacle : obstacles) {
+        point_t direction = std::get<0>(obstacle);
+        point_t point = std::get<1>(obstacle);
+
+        QPVector2D direction_vec{std::get<0>(direction), std::get<1>(direction)};
+        QPVector3D point_vec{std::get<0>(point), std::get<1>(point), std::get<2>(point)};
+
+        horizontal_lines.push_back(HorizontalLine(direction_vec, point_vec));
+    }
+    return horizontal_lines;
+}
+
 std::tuple<std::vector<std::array<float, 6>>, std::vector<std::array<float, 6>>, std::vector<std::array<float, 6>>> solve_1(
     std::array<float, 6> start_pos_joints, 
     std::array<float, 6> end_pos_joints, 
@@ -44,11 +71,11 @@ std::tuple<std::vector<std::array<float, 6>>, std::vector<std::array<float, 6>>,
     constraint_t position_constraints,
     std::vector<std::tuple<point_t, point_t>> obstacles) {
 
-        constraints::Constraint<6> velocity_constraints_transformed{std::get<0>(velocity_constraints), std::get<1>(velocity_constraints)};
-        constraints::Constraint<6> acceleration_constraints_transformed{std::get<0>(acceleration_constraints), std::get<1>(acceleration_constraints)};
-        constraints::Constraint<6> position_constraints_transformed{std::get<0>(position_constraints), std::get<1>(position_constraints)};
-
-
+        constraints::Constraint<N_DIM> velocity_constraints_transformed = createConstraint(velocity_constraints);
+        constraints::Constraint<N_DIM> acceleration_constraints_transformed = createConstraint(acceleration_constraints);
+        constraints::Constraint<N_DIM> position_constraints_transformed = createConstraint(position_constraints);
+        
+        
 
         return {{{start_pos_joints.at(0),2,3,4,5,6}}, {{1,2,3,4,5,6}}, {{1,2,3,4,5,6}}};
     }
